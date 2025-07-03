e00eefa7308c85775c871a7d607a3f0b862b97ab
umesh-air <inbho@outlook.com>
2025-07-03 21:46:25 +0545

    adding the component

---
+++ src/component/header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+               <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Resume</a></li>
+                <li><a href="#">Docs</a></li>
+                <li><a href="#">Contact</a></li>
+               </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
+
